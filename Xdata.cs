1ba965e4702a47f58da6359d134030ade98f652c
AdsonAraujo917 <adson.araujo@tpfe.com.br>
2025-07-23 08:38:15 -0300

    Add files via upload

---
+++ Xdata.cs
@@ -0,0 +1,89 @@
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicCivilTests
+{
+    internal class Xdata
+    {
+        private static bool createRegAppTableRecord(string appName, Transaction transaction, Database database)
+        {
+            RegAppTable regAppTable = (RegAppTable)transaction.GetObject(database.RegAppTableId, OpenMode.ForRead);
+            if (!regAppTable.Has(appName))
+            {
+                using (RegAppTableRecord regAppTableRecord = new RegAppTableRecord())
+                {
+                    regAppTable.UpgradeOpen();
+                    regAppTableRecord.Name = appName;
+                    transaction.GetObject(database.RegAppTableId, OpenMode.ForWrite);
+                    regAppTable.Add(regAppTableRecord);
+                    transaction.AddNewlyCreatedDBObject(regAppTableRecord, true);
+                }
+            }
+            return true;
+        }
+
+        public static void SetXData(string appName, int value, Database database, Transaction transaction, DBObject dbObject)
+        {
+            Xdata.createRegAppTableRecord(appName, transaction, dbObject.Database);
+            using (ResultBuffer resultBuffer = new ResultBuffer())
+            {
+                resultBuffer.Add(new TypedValue((int)DxfCode.ExtendedDataRegAppName, appName));
+                resultBuffer.Add(new TypedValue((int)DxfCode.ExtendedDataInteger32, value));
+                dbObject.XData = resultBuffer;
+            }
+        }
+
+        public static void SetXData(string appName, List<Tuple<string, dynamic, int>> data, Transaction transaction, DBObject dbObject)
+        {
+            Database db = dbObject.Database;
+            ResultBuffer resultbuffer = new ResultBuffer(new TypedValue((int)DxfCode.ExtendedDataRegAppName, appName));
+            Xdata.createRegAppTableRecord(appName, transaction, dbObject.Database);
+            using (ResultBuffer resultBuffer = new ResultBuffer())
+            {
+                foreach (Tuple<string, dynamic, int> item in data)
+                {
+                    resultbuffer.Add(new TypedValue((int)DxfCode.ExtendedDataAsciiString, item.Item1));
+                    resultbuffer.Add(new TypedValue(item.Item3, item.Item2));
+                }
+                dbObject.XData = resultbuffer;
+                resultbuffer.Dispose();
+            }
+        }
+
+        public static void SetXData(string appName, Handle value, Database database, Transaction transaction, DBObject dbObject)
+        {
+            Xdata.createRegAppTableRecord(appName, transaction, dbObject.Database);
+            using (ResultBuffer resultBuffer = new ResultBuffer())
+            {
+                resultBuffer.Add(new TypedValue((int)DxfCode.ExtendedDataRegAppName, appName));
+                resultBuffer.Add(new TypedValue((int)DxfCode.ExtendedDataHandle, value));
+                dbObject.XData = resultBuffer;
+            }
+        }
+
+        public static void SetXData(string appName, string value, Database database, Transaction transaction, DBObject dbObject)
+        {
+            Xdata.createRegAppTableRecord(appName, transaction, dbObject.Database);
+            using (ResultBuffer resultBuffer = new ResultBuffer())
+            {
+                resultBuffer.Add(new TypedValue((int)DxfCode.ExtendedDataRegAppName, appName));
+                resultBuffer.Add(new TypedValue((int)DxfCode.ExtendedDataAsciiString, value));
+                dbObject.XData = resultBuffer;
+            }
+        }
+
+        public static String[] GetXData(string appName, DBObject dbObject) 
+        {
+            ResultBuffer resultBuffer = dbObject.GetXDataForApplication(appName);
+            List<String> stringList = new List<string> { };
+            foreach (TypedValue typedValue in resultBuffer.AsArray()) {
+                stringList.Add(typedValue.Value.ToString());
+            }
+            return stringList.ToArray();
+        }
+    }
+}
