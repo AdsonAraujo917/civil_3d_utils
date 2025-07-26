25cf5b1e91a76284a298676c102b0dc051e25d96
AdsonAraujo917 <adson.araujo@tpfe.com.br>
2025-07-26 12:47:04 -0300

    Update geometry.cs

--- geometry.cs
+++ geometry.cs
@@ -1,1 +1,10 @@
-
+public static class Geometry
+{
+    public static double GetTriangleArea(Point3d p1, Point3d p2, Point3d p3) 
+    {
+        Vector3d ab = p2 - p1;
+        Vector3d ac = p3 - p1;
+        Vector3d cross = ab.CrossProduct(ac);
+        return 0.5 * cross.Length;
+    }
+}
